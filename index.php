38667faa9d7f744067996ee67a1b6520c5b9e1c5
namous68 <vnesys@gmail.com>
2023-07-17 15:51:51 +0200

    creation

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/main.css">
+    <title>spoty-Clone</title>
+</head>
+
+<body>
+    <section class="sec1">
+        <div class="head">
+            <button class="btn btn-primary">Accueil</button><br><br>
+            <button class="btn btn-primary">connexion</button><br><br>
+            <input class="recherche" placeholder="Recherche" type="text">
+        </div>
+       
+    </section>
+    
+    <section>
+    <h2>album</h2>
+    <div class="container">
+      <div class="box">
+        <img src="./img/bellie eilish.jpg" alt="">
+        <p>Bellie Eilish</p>
+      </div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+     
+      
+    </div>
+  </section>
+    
+  <section>
+    <h2>playlist</h2>
+    <div class="container">
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+    </div>
+  </section>
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ partiels/playlist.php
@@ -0,0 +1,10 @@
+<section>
+    <h2>playlist</h2>
+    <div class="container">
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+    </div>
+  </section>
