47306e1d1732064c45322d1552ba11bcfee8b965
Amayas-Tamarat <81672964+Amayas-Tamarat@users.noreply.github.com>
2023-07-17 13:45:35 +0200

    Update spotify.sql

--- connect/spotify.sql
+++ connect/spotify.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 17 juil. 2023 à 09:37
+-- Généré le : lun. 17 juil. 2023 à 11:45
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
@@ -33,7 +33,14 @@ CREATE TABLE IF NOT EXISTS `album` (
   `title` varchar(8000) DEFAULT NULL,
   `img` varchar(8000) DEFAULT NULL,
   PRIMARY KEY (`id_album`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `album`
+--
+
+INSERT INTO `album` (`id_album`, `title`, `img`) VALUES
+(1, 'Test1', 'twilight.jpg');
 
 -- --------------------------------------------------------
 
@@ -64,7 +71,14 @@ CREATE TABLE IF NOT EXISTS `musique` (
   `id_album` int NOT NULL,
   PRIMARY KEY (`id_musique`),
   KEY `id_album` (`id_album`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `musique`
+--
+
+INSERT INTO `musique` (`id_musique`, `path`, `title`, `id_album`) VALUES
+(1, '../audio/sample.mp3', 'Test', 1);
 
 -- --------------------------------------------------------
 
@@ -90,8 +104,16 @@ DROP TABLE IF EXISTS `playlist`;
 CREATE TABLE IF NOT EXISTS `playlist` (
   `id_playlist` int NOT NULL AUTO_INCREMENT,
   `tilte` varchar(8000) DEFAULT NULL,
+  `img` varchar(300) NOT NULL,
   PRIMARY KEY (`id_playlist`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `playlist`
+--
+
+INSERT INTO `playlist` (`id_playlist`, `tilte`, `img`) VALUES
+(1, 'Test', '');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
