de397c7f64bf6e17bd787fef7f0ba8d78fdde7e4
Amayas-Tamarat <amayas.tamarat@gmail.com>
2023-07-20 10:18:15 +0200

    playlist creation done

--- affichage/createPlaylist.php
+++ affichage/createPlaylist.php
@@ -1,17 +1,33 @@
 <?php
 require_once("../connect/connect.php");
-$idAlbum = $_GET['id'];
-$sql = ("SELECT id_playlist FROM playlist WHERE id_playlist = :idAlbum ");
+$idPlaylist = $_GET['id'];
+$sql = ("SELECT tilte FROM playlist WHERE id_playlist = :idPlaylist ");
 $query = $db->prepare($sql);
-$query->bindValue(':idAlbum',$idAlbum, PDO::PARAM_INT);
+$query->bindValue(':idPlaylist',$idPlaylist, PDO::PARAM_INT);
 $query->execute();
-$albums = $query->fetchAll(PDO::FETCH_ASSOC);
-if ($albums == false) {
-    echo "ye";
-}else
-foreach ($albums as $album ) {
-    var_dump($album);
+$playlist = $query->fetchAll(PDO::FETCH_ASSOC);
+if ($playlist == false) {
+    if(isset($_POST)){
+    if(
+        isset($_POST['playlistTitle']) && !empty($_POST['playlistTitle'])
+        && isset($_POST['playlistImg']) && !empty($_POST['playlistImg'])
+        ){
+            $dateHour = strip_tags($_POST['dateHour']);
+            $idPatients = strip_tags($_POST['idPatients']);
+            $sql = "INSERT INTO `playlist` ( `title`, `img`) 
+                    VALUES ( :playlistTitle, :playlistImg);";
+            $query = $db->prepare($sql);
+            $query->bindValue(':playlistImg', $playlistImg, PDO::PARAM_STR);
+            $query->bindValue(':playlistTitle', $playlistTitle, PDO::PARAM_STR);
+            $query->execute();
+            header('Location: index.php');
+        }
+    }
+    }  
+ else {
+    echo "cette playlist existe deja.";
 }
+
 
 
 
@@ -30,5 +46,4 @@ foreach ($albums as $album ) {
 //             $query->execute();
 //             header('Location: index.php');
 //         }
-// }
-?>
+//     }
---
+++ s
@@ -0,0 +1,9 @@
+  album[m
+  commentaire[m
+  comments[m
+  createPlaylist[m
+  createcom[m
+  main[m
+  newcom[m
+  playlist[m
+* [32mplaylistcreate[m
